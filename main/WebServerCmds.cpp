edc0e4943ef6685cd0912020075f1f4bef4ca308
labelette <pte@a.com>
2023-01-25 18:59:54 +0100

    resolve rebase probleme
    merge
    
    update automaticaly the device id of sensor that battery as been replaced.
    
    algo
    
    if a new device is detected with batery level 100% (new batery)
     we search if a same device type/subtype/Unit already exist with batery low
    if one is found , we assume that it is the same device that we have replace batery and a new ID as been modified.
    the old DeviceID is replaced by the new DeviceID
    to allow this , the settings option  other/"Allow Device Replace On Battery Changed Detected" shall be ON

--- main/WebServerCmds.cpp
+++ main/WebServerCmds.cpp
@@ -3999,6 +3999,12 @@ namespace http
 					m_sql.CheckBatteryLow();
 				cntSettings++;
 
+				//update AllowDeviceReplaceOnBatteryChanged
+				std::string AllowDeviceReplaceOnBatteryChanged = request::findValue(&req, "AllowDeviceReplaceOnBatteryChanged");
+				int iAllowDeviceReplaceOnBatteryChanged = (AllowDeviceReplaceOnBatteryChanged == "on" ? 1 : 0);
+				m_sql.UpdatePreferencesVar("AllowDeviceReplaceOnBatteryChanged", iAllowDeviceReplaceOnBatteryChanged);
+				cntSettings++;
+
 				/* Update the Theme */
 
 				std::string SelectedTheme = request::findValue(&req, "Themes");
@@ -6780,6 +6786,10 @@ namespace http
 				else if (Key == "IFTTTEnabled")
 				{
 					root["IFTTTEnabled"] = nValue;
+				}
+				else if (Key == "AllowDeviceReplaceOnBatteryChanged")
+				{
+					root["AllowDeviceReplaceOnBatteryChanged"] = nValue;
 				}
 				else if (Key == "IFTTTAPI")
 				{
