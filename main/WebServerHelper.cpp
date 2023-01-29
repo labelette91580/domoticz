04f45930e9105359ed49e9c204c88f6f517748b1
labelette <pte@a.com>
2023-01-29 10:46:51 +0100

    merge imperihome

--- main/WebServerHelper.cpp
+++ main/WebServerHelper.cpp
@@ -108,6 +108,20 @@ namespace http {
 				it->ClearUserPasswords();
 			}
 		}
+		void CWebServerHelper::RType_Scenes(Json::Value &root)//called from imperihome
+		{
+			request request_;
+			WebEmSession session ;
+
+			if (plainServer_) { // assert
+				plainServer_->RType_Scenes(session,request_,root);
+			}
+#ifdef NS_ENABLE_SSL
+			else if (secureServer_) {
+				secureServer_->RType_Scenes(session,request_,root);
+			}
+#endif
+		}
 
 		void CWebServerHelper::ReloadTrustedNetworks()
 		{
