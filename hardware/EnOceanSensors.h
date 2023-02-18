b18610434757720b57319f98bd677a4b18b2429c
labelette <pte@a.com>
2023-02-18 19:56:53 +0100

    correct error

--- hardware/EnOceanSensors.h
+++ hardware/EnOceanSensors.h
@@ -57,7 +57,7 @@ namespace enocean {
 		bool UnLockTimeout();
 		void SetUnLockTimeout();
 		void ReSetUnLockTimeout();
-		std::string NodeInfo::Description();
+		std::string Description();
 		static std::string teachin_mode_string(TeachinMode pteachin_mode);
 		std::string teachin_mode_string();
 		void setLinkTableMedadata( int csize, int MaxSize);
