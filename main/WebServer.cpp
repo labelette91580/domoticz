bcf83305258aab4571553d9d52d641d2266c8ab7
labelette <pte@a.com>
2023-02-25 11:50:08 +0100

    add setlogoption

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -728,6 +728,15 @@ namespace http
 			// EnOcean ESP3 Rtypes
 			RegisterRType("esp3getnodes", [this](auto&& session, auto&& req, auto&& root) { RType_EnOceanESP3GetNodes(session, req, root); });
 
+			RegisterCommandCode("setlogoption", [this](auto &&session, auto &&req, auto &&root)  {
+				//http://192.168.1.27:8080/json.htm?type=command&param=setlogoption&logflags=normal,status,error,debug
+				//http://192.168.1.6:8080/json.htm?type=command&param=setlogoption&filter=Send:_,Read_,HwdID,%2bRMC,%2bWait:,SQLH,IMPA,RFX,dzVents,&debugflags=normal,hardware,received_,auth_,webserver_
+				root["status"] = "OK";root["title"] = "setlogoption";
+				std::string option = request::findValue(&req, "filter");if (!option.empty() )_log.SetFilter(option);
+				option = request::findValue(&req, "logflags");if (!option.empty() )_log.SetLogFlags(option);
+				option = request::findValue(&req, "debugflags");if (!option.empty() )_log.SetDebugFlags(option);
+			});
+
 			//Whitelist
 			m_pWebEm->RegisterWhitelistURLString("/images/floorplans/plan");
 
