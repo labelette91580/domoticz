bbe05360ca06660658714ec1839bdf9fd89fecf2
labelette <pte@a.com>
2024-03-28 15:15:42 +0100

    update the log only if the value are different from last or at least every hour move CleanupShortLog from ScheduleShortlog() to ScheduleDay() set value to 0 for <0 value of week/month /year historical graph

--- main/WebServerHandleGraph.cpp
+++ main/WebServerHandleGraph.cpp
@@ -1716,6 +1716,9 @@ namespace http
 							{
 								root["result"][ii]["d"] = sd[1].substr(0, 16);
 								std::string szValue = sd[0];
+								if (atof(szValue.c_str())<0)
+									szValue="0";
+
 								switch (metertype)
 								{
 								case MTYPE_ENERGY:
@@ -3222,6 +3225,8 @@ namespace http
 									root["result"][ii]["d"] = sd[1].substr(0, 16);
 
 									std::string szValue = sd[0];
+									if (atof(szValue.c_str())<0)
+										szValue="0";
 
 									double fcounter = atof(sd[2].c_str());
 
