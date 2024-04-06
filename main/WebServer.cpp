2f1784ad92858fa799cf3a739aad10577cff566b
labelette <pte@a.com>
2024-04-06 17:55:11 +0200

    resolve merge conflict with log

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -594,6 +594,9 @@ namespace http
 #endif
 
 			// EnOcean helpers cmds
+			extern void RType_OpenEnOcean(http::server::WebEmSession& session, const http::server::request& req, Json::Value& root);
+			RegisterCommandCode("enocean", [this](auto &&session, auto &&req, auto &&root)  { RType_OpenEnOcean(session, req, root); });
+
 			RegisterCommandCode("enoceangetmanufacturers", [this](auto&& session, auto&& req, auto&& root) { Cmd_EnOceanGetManufacturers(session, req, root); });
 			RegisterCommandCode("enoceangetrorgs", [this](auto&& session, auto&& req, auto&& root) { Cmd_EnOceanGetRORGs(session, req, root); });
 			RegisterCommandCode("enoceangetprofiles", [this](auto&& session, auto&& req, auto&& root) { Cmd_EnOceanGetProfiles(session, req, root); });
@@ -608,10 +611,6 @@ namespace http
 			RegisterCommandCode("esp3updatenode", [this](auto&& session, auto&& req, auto&& root) { Cmd_EnOceanESP3UpdateNode(session, req, root); });
 			RegisterCommandCode("esp3deletenode", [this](auto&& session, auto&& req, auto&& root) { Cmd_EnOceanESP3DeleteNode(session, req, root); });
 			RegisterCommandCode("esp3getnodes", [this](auto&& session, auto&& req, auto&& root) { Cmd_EnOceanESP3GetNodes(session, req, root); });
-
-			extern void RType_OpenEnOcean(http::server::WebEmSession& session, const http::server::request& req, Json::Value& root);
-			RegisterCommandCode("enocean", [this](auto &&session, auto &&req, auto &&root)  { RType_OpenEnOcean(session, req, root); });
-
 
 			//Whitelist
 			m_pWebEm->RegisterWhitelistURLString("/images/floorplans/plan");
