abc0427472c23269fbbe271eb63bc959a2def11f
labelette91580 <pte@hotmail.com>
2025-11-10 19:22:11 +0100

    printRawDataValues on one line

--- hardware/EnOceanRawValue.cpp
+++ hardware/EnOceanRawValue.cpp
@@ -216,7 +216,8 @@ namespace enocean
 		{
 			uint32_t value = GetRawValue(data, OffsetDes);
 //			snprintf(line, sizeof(line) - 1, "offset:%2d = %5d (%08X)=%-20s : %s : %s ", OffsetDes->Offset, value, value, OffsetDes->getEnumerate(value).c_str(), OffsetDes->ShortCut.c_str(), OffsetDes->description.c_str());
-			snprintf(line, sizeof(line) - 1, "offs:%2d [%s:%s]=[%d(%X):%s] / ", OffsetDes->Offset, OffsetDes->ShortCut.c_str(), OffsetDes->description.c_str() , value, value, OffsetDes->getEnumerate(value).c_str() );
+//			snprintf(line, sizeof(line) - 1, "offs:%2d [%s:%s]=[%d(%X):%s] / ", OffsetDes->Offset, OffsetDes->ShortCut.c_str(), OffsetDes->description.c_str() , value, value, OffsetDes->getEnumerate(value).c_str() );
+			snprintf(line, sizeof(line) - 1, "%2d:[%s:%s]=[%d:%s]/", OffsetDes->Offset, OffsetDes->ShortCut.c_str(), OffsetDes->description.c_str() , value, OffsetDes->getEnumerate(value).c_str() );
 			message += line;
 			OffsetDes++;
 		}
