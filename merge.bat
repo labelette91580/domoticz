f3eda249c36454d7aaab00abdd8728b159b30c53
labelette91580 <pte@hotmail.com>
2025-12-09 19:09:51 +0100

    add message

--- merge.bat
+++ merge.bat
@@ -1,20 +1,20 @@
-"C:\Program Files\Git\bin\"git merge  UpdateToggleName
-pause
-"C:\Program Files\Git\bin\"git merge OnlyUpdateNewValue
-pause
-"C:\Program Files\Git\bin\"git merge RawRfx
-pause
-"C:\Program Files\Git\bin\"git merge  BatteryAutomaticReplace
-pause
-"C:\Program Files\Git\bin\"git merge  scripteHumidity
-pause
-"C:\Program Files\Git\bin\"git merge  LogFilter
-pause
-"C:\Program Files\Git\bin\"git merge  imperihome1
-pause
-"C:\Program Files\Git\bin\"git merge  Enocean4
-pause
-"C:\Program Files\Git\bin\"git merge  CopyPlanning
+"C:\Program Files\Git\bin\"git merge  UpdateToggleName         -m " merge UpdateToggleName        "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  OnlyUpdateNewValue       -m " merge OnlyUpdateNewValue      "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  RawRfx                   -m " merge RawRfx                  "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  BatteryAutomaticReplace  -m " merge BatteryAutomaticReplace "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  scripteHumidity          -m " merge scripteHumidity         "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  LogFilter                -m " merge LogFilter               "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  imperihome1              -m " merge imperihome1             "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  Enocean4                 -m " merge Enocean4                "
+pause                                                          -m " merge                         "
+"C:\Program Files\Git\bin\"git merge  CopyPlanning             -m " merge CopyPlanning            "
 pause
 
 
